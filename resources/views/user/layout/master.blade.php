45dc1d3d4004e966ec6d0a92841f775be5d4a528
Pathakkriishna <kriishna.pathak@gmail.com>
2023-02-24 16:24:09 +0545

    admin authentication

--- resources/views/user/layout/master.blade.php
+++ resources/views/user/layout/master.blade.php
@@ -7,13 +7,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>Artihc</title>
     <!-- normalize css -->
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.min.css"
-        integrity="sha512-NhSC1YmyruXifcj/KFRWoC561YpHpc5Jtzgvbuzx5VozKpWvQ+4nXhPdFgmx8xqexRcpAglTj9sIBWINXa8x5w=="
-        crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.min.css" integrity="sha512-NhSC1YmyruXifcj/KFRWoC561YpHpc5Jtzgvbuzx5VozKpWvQ+4nXhPdFgmx8xqexRcpAglTj9sIBWINXa8x5w==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <!-- font awesome cdn -->
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta2/css/all.min.css"
-        integrity="sha512-YWzhKL2whUzgiheMoBFwW8CKV4qpHQAEuvilg9FAn5VJUDwKZZxkJNuGM4XkWuk94WCrrwslk8yWNGmY1EduTA=="
-        crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta2/css/all.min.css" integrity="sha512-YWzhKL2whUzgiheMoBFwW8CKV4qpHQAEuvilg9FAn5VJUDwKZZxkJNuGM4XkWuk94WCrrwslk8yWNGmY1EduTA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <!-- custom utilities css -->
     <link rel="stylesheet" href="{{asset('userpanel/css/utilities.css')}}" />
     <!-- custom main css -->
@@ -23,11 +19,12 @@
     <!-- slick slider css -->
     <link rel="stylesheet" href="{{asset('userpanel/plugins/slick-1.8.1/slick/slick.css')}}" />
     <link rel="stylesheet" href="{{asset('userpanel/plugins/slick-1.8.1/slick/slick-theme.css')}}" />
-    
-  
+
+
 </head>
 
 <body>
+
     <div class="holder">
         <!-- navbar -->
         @include('user.layout.nav')
@@ -48,7 +45,12 @@
     <script src="{{asset('userpanel/plugins/slick-1.8.1/slick/slick.js')}}"></script>
     <!-- custom js -->
     <script src="{{asset('userpanel/js/script.js')}}"></script>
-   
+    @if(session()->has('error_msg'))
+    <script>
+        alert("{{ session('error_msg') }}");
+    </script>
+    @endif
+
 </body>
 
 </html>
