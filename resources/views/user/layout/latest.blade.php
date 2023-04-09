03c1608798be360b8b6cc483a30b2aedbb9d87bd
anupzpt <prajapatianu433@gmail.com>
2023-04-09 20:32:13 +0545

    for merge

--- resources/views/user/layout/latest.blade.php
+++ resources/views/user/layout/latest.blade.php
@@ -1,7 +1,7 @@
 <section class="featured-deals bg-light-grey-color-shade py" id="featured-deals">
     <div class="container">
         <div class="section-title text-center">
-            <h2>Latest Art</h2>
+            <h2>Latest Arts</h2>
             <p class="lead">
                 Collection of cutting-edge artwork from emerging artists.
             </p>
@@ -14,14 +14,12 @@
                     <div class="myimg">
                         <img src="{{ asset('/uploads' . '/' . $latest->image) }}" />
                     </div>
-
                     <div class="info bg-dark text-white">
                         <div class="text-center">
                             <p class="text-capitalize mt-3 mb-1">{{ $latest->name }}</p>
                             <span class="fw-bold d-block">Rs. {{ $latest->price }}</span>
                             <a onClick="buyNow('{{ $latest->id }}')" class="button btn-primary mt-3 ml-2">Buy it
                                 Now</a>
-
                         </div>
                     </div>
                 </div>
